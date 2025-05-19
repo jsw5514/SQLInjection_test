aceab52dbbad4f6d6753edab0feb2e8d81d4f7ee
jsw5514 <jsw5524@gmail.com>
2025-05-20 00:46:37 +0900

    로그인 함수를 하나로 사용하는 대신 로그인 모드를 전달받아 로그인 구현방식을 선택할 수 있게 수정

--- server/src/main/java/com/sqlinjectiontest/server/Service/UserService.java
+++ server/src/main/java/com/sqlinjectiontest/server/Service/UserService.java
@@ -2,6 +2,8 @@ package com.sqlinjectiontest.server.Service;
 
 import com.sqlinjectiontest.server.Entity.UsersEntity;
 import com.sqlinjectiontest.server.Repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,13 @@ import java.util.List;
 
 @Service
 public class UserService {
+    Logger log = LoggerFactory.getLogger(UserService.class); 
+    //로그인 모드 지정 상수
+    public final int LOGIN_MODE_NATIVE = 0;
+    public final int LOGIN_MODE_FILTERED = 1;
+    public final int LOGIN_MODE_PARAM_BINDING = 2;
+    public final int LOGIN_MODE_JPA = 3;
+    
     private final UserRepository userRepository;
     
     @Autowired
@@ -16,16 +25,40 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    /** 네이티브 sql 쿼리를 이용해 로그인하는 함수(sql 삽입에 취약)
+    /** 로그인 함수
      * @param id id
      * @param password 비밀번호
+     * @param loginMode 로그인 구현 방식을 지정(상수로 선언된 값 사용)
+     *                  LOGIN_MODE_NATIVE: sql 삽입에 취약한 문자열 연결 방식 그대로 사용
+     *                  LOGIN_MODE_FILTERED: 파라미터 필터링 후 문자열 연결 사용
+     *                  LOGIN_MODE_PARAM_BINDING: 파라미터 바인딩을 이용(PreparedStatement)
+     *                  LOGIN_MODE_JPA: JPA 이용
      * @return 로그인 성공여부
      */
-    public boolean loginNative(String id, String password) {
-        List<UsersEntity> users = userRepository.getUsersByIdAndPassword(id, password);
-        if (users.size() == 0) {
+    public boolean login(String id, String password, int loginMode) {
+        List<UsersEntity> users = null;
+        switch (loginMode) {
+            case LOGIN_MODE_FILTERED:
+                //TODO filtering
+                break;
+            case LOGIN_MODE_NATIVE:
+                users = userRepository.getUsersByIdAndPassword(id, password);
+                break;
+            case LOGIN_MODE_PARAM_BINDING:
+                //TODO parameter binding
+            case LOGIN_MODE_JPA:
+                //TODO use jpa
+                break; 
+            default:
+                log.error("Invalid login mode");
+                throw new IllegalStateException("Invalid login mode");
+        }
+        
+        if (users == null || users.isEmpty()) {
             return false;
         }
-        else return true;
+        else {
+            return true;
+        }
     }
 }
