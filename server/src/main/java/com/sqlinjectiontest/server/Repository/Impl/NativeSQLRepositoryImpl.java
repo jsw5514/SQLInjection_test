7059798a09f1b63f4f8aa72763e88848f7151d49
jsw5514 <jsw5524@gmail.com>
2025-05-20 00:14:19 +0900

    네이티브 쿼리 사용을 위한 리포지토리 추가 및 구현

---
+++ server/src/main/java/com/sqlinjectiontest/server/Repository/Impl/NativeSQLRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.sqlinjectiontest.server.Repository.Impl;
+
+import com.sqlinjectiontest.server.Entity.UsersEntity;
+import com.sqlinjectiontest.server.Repository.NativeSQLRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.List;
+
+public class NativeSQLRepositoryImpl implements NativeSQLRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    /** SQL 삽입에 취약한 로그인용 함수
+     * @param id id
+     * @param password 비밀번호
+     * @return 입력정보와 일치하는 유저 리스트
+     */
+    @Override
+    public List<UsersEntity> getUsersByIdAndPassword(String id, String password) {
+        String sql = "select * from users where id = " + id + " and password = " + password;
+        List<UsersEntity> result = entityManager.createNativeQuery(sql).getResultList();
+        return result;
+    }
+}
--- server/src/main/java/com/sqlinjectiontest/server/Repository/UserRepository.java
+++ server/src/main/java/com/sqlinjectiontest/server/Repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.sqlinjectiontest.server.Repository;
 import com.sqlinjectiontest.server.Entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UsersEntity, String> {
+public interface UserRepository extends JpaRepository<UsersEntity, String>, NativeSQLRepository {
 }
