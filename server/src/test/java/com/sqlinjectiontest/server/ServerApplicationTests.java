938bb7a08398aeb4c193cc707684fca8a7103344
jsw5514 <jsw5524@gmail.com>
2025-05-20 11:29:21 +0900

    테스트 코드 추가

--- server/src/test/java/com/sqlinjectiontest/server/ServerApplicationTests.java
+++ server/src/test/java/com/sqlinjectiontest/server/ServerApplicationTests.java
@@ -1,13 +1,49 @@
 package com.sqlinjectiontest.server;
 
+import com.sqlinjectiontest.server.Service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ServerApplicationTests {
+import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+public class ServerApplicationTests {
+	
+	@Autowired
+	private UserService userService;
+	
 	@Test
-	void contextLoads() {
+	void loginTest() {
+		//test nomal login
+		doNomalLogin("admin","adminpass");
+		
+		//test SQL injection
+		doSQLInjection("admin\"--","asdf");
+	}
+
+	void doNomalLogin(String id, String password) {
+		boolean result;
+		result = userService.login(id,password,UserService.LOGIN_MODE_NATIVE);
+		assertTrue(result);
+		result = userService.login(id, password, UserService.LOGIN_MODE_FILTERED);
+		assertTrue(result);
+		result = userService.login(id, password, UserService.LOGIN_MODE_PARAM_BINDING);
+		assertTrue(result);
+		result = userService.login(id, password, UserService.LOGIN_MODE_JPA);
+		assertTrue(result);
+	}
+
+	void doSQLInjection(String id, String password) {
+		boolean result;
+		result = userService.login(id,password,UserService.LOGIN_MODE_NATIVE);
+		assertTrue(result);
+		result = userService.login(id, password, UserService.LOGIN_MODE_FILTERED);
+		assertFalse(result);
+		result = userService.login(id, password, UserService.LOGIN_MODE_PARAM_BINDING);
+		assertFalse(result);
+		result = userService.login(id, password, UserService.LOGIN_MODE_JPA);
+		assertFalse(result);
 	}
 
 }
